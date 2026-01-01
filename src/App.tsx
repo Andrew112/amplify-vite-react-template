0cc34306efe3807b41aa9ca29f3419ef5093e38e
Andrew112 <andrew.coleman330@gmail.com>
2025-12-31 19:06:54 -0600

    aws changes authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
+
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
+
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -17,14 +21,22 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
+      <button onClick={signOut}>Sign Out</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
